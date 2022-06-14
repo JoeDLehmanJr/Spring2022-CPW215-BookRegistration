292f7ae86641641a0f7a58ebc761e1c7ab3b8169
JoeLehmanJr <joejr65@live.com>
2022-06-14 11:10:05 -0700

    Created and database connection method to save on code redundancy.

---
+++ BookRegistration/DBHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+
+namespace BookRegistration
+{
+    /// <summary>
+    /// Helper method which simplifies the connection to the database 
+    /// </summary>
+    public class DBHelper
+    {
+        /// <summary>
+        /// Helper method which simplifies the connection to the database 
+        /// </summary>
+        /// <returns>A Sql Connection to the database</returns>
+        public static SqlConnection GetConnection()
+        {
+            // Establish connection to database
+            return new SqlConnection("Data Source = localhost; Initial Catalog = BookRegistration; Integrated Security = True");
+        }
+    }
+}
