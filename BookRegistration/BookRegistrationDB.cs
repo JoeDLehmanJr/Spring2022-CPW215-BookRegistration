aacc0dad8cfa1858361910df5ca0ef5a6bb78d38
JoeLehmanJr <joejr65@live.com>
2022-06-14 11:04:49 -0700

    Finished both the Registration class and BookRegistrationDB class.

--- BookRegistration/BookRegistrationDB.cs
+++ BookRegistration/BookRegistrationDB.cs
@@ -1,35 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Data.SqlClient;
 
 namespace BookRegistration
 {
+    /// <summary>
+    /// This method assists with adding and getting Book reservation information from a T-SQL database.
+    /// </summary>
     public class BookRegistrationDB
     {
-        public void Add(Registration registration)
+        /// <summary>
+        /// Adds a Book reservation information (customerId, ISBN, and reservation date) to the database.
+        /// </summary>
+        /// <param name="reg">A reservation object(customerId, ISBN, and reservation date).</param>
+        /// <returns>True if the addition was successful, false if not.</returns>
+        public static bool RegisterBook(Registration reg)
         {
-            throw new System.NotImplementedException();
-        }
+            // Get Connection
+            using SqlConnection con = DBHelper.GetConnection();
+            // Prepare insert statement
+            SqlCommand insertCmd = new();
+            insertCmd.Connection = con;
 
-        public void Delete(Registration registration)
-        {
-            throw new System.NotImplementedException();
-        }
+            // Parameterized query
+            insertCmd.CommandText = "Insert Into Registration(CustomerID, ISBN, RegDate)" +
+                "Values (@CustomerID, @ISBN, @RegDate)";
 
-        public System.Data.SqlClient.SqlConmection GetBookRegistrationDBConnection()
-        {
-            throw new System.NotImplementedException();
-        }
+            insertCmd.Parameters.AddWithValue("@CustomerID", reg.CustomerID);
+            insertCmd.Parameters.AddWithValue("@ISBN", reg.Isbn);
+            insertCmd.Parameters.AddWithValue("@RegDate", reg.RegDate);
 
-        public System.Collections.Generic.List<Registration> GetAllRegistrations()
-        {
-            throw new System.NotImplementedException();
-        }
+            // Open connection to the database
+            con.Open();
 
-        public void Update(Registration registration)
-        {
-            throw new System.NotImplementedException();
+            // Execute insert query
+            int rows = insertCmd.ExecuteNonQuery();
+            if (rows > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- BookRegistration/Registration.cs
+++ BookRegistration/Registration.cs
@@ -5,43 +5,47 @@ using System.Text;
 
 namespace BookRegistration
 {
+    /// <summary>
+    /// Creates a book registration object with three characteristics: the isbn, the CustomerId, and the registration date.
+    /// </summary>
     public class Registration
     {
-        public Registration(System.DateTime registrationDate, string isbn, int customerId)
-        {
-            throw new System.NotImplementedException();
-        }
+        private int _customerID;
+        private string _isbn;
 
+        /// <summary>
+        /// Constructor for creating the book registration object
+        /// </summary>
+        /// <param name="custId">The customer identification number to uniquely identify an individual.</param>
+        /// <param name="isbn">The international standard book number, is used to uniquely identify a single book.</param>
+        /// <param name="registrationDate">The date the book was checked out.</param>
+        public Registration(int custId, string isbn, DateTime registrationDate)
+        {
+            this.CustomerID = custId;
+            this.Isbn = isbn;
+            this.RegDate = registrationDate;
+        }
+        /// <summary>
+        /// Getter and setter for the _customerID field.
+        /// </summary>
         public int CustomerID
         {
-            get => default;
-            set
-            {
-            }
+            get { return _customerID; }
+            set { _customerID = value; }
         }
 
-        public int ISBN
+        /// <summary>
+        /// Getter and setter for the _isbn field.
+        /// </summary>
+        public String Isbn
         {
-            get => default;
-            set
-            {
-            }
+            get { return _isbn; }
+            set { _isbn = value; }
         }
 
-        public System.DateTime RegDate
-        {
-            get => default;
-            set
-            {
-            }
-        }
-
-        public overrideToString()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        private int _isbn;
-        private int _customerID;
+        /// <summary>
+        /// Getter and setter for the RegDate attribute.
+        /// </summary>
+        public DateTime RegDate { get; set; }
     }
 }
